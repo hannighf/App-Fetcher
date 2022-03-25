0dbcdab3e8223e44ac605f52dc6c803534c3de58
hannighf <hannighf@gmail.com>
2022-03-25 20:08:52 +0900

    add bringing an app to any position and any size

--- App-Fetcher/ViewController.swift
+++ App-Fetcher/ViewController.swift
@@ -6,13 +6,21 @@
 //
 
 import Cocoa
+import AppKit
 
 class ViewController: NSViewController {
-
+    
+    @IBOutlet weak var x: NSTextField!
+    @IBOutlet weak var y: NSTextField!
+    @IBOutlet weak var width: NSTextField!
+    @IBOutlet weak var height: NSTextField!
+    @IBOutlet weak var useCostomPoint: NSButton!
+    @IBOutlet weak var useCostomSize: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let appDelegate:AppDelegate = NSApplication.shared.delegate as! AppDelegate
+        appDelegate.viewController = self
     }
 
     override var representedObject: Any? {
@@ -20,7 +28,25 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
+    
+    @IBAction func pointCheckBoxClicked(_ sender: Any) {
+        if useCostomPoint.state != .on {
+            x.isEnabled = false
+            y.isEnabled = false
+        } else {
+            x.isEnabled = true
+            y.isEnabled = true
+        }
+    }
+    
+    @IBAction func sizeCheckBoxClicked(_ sender: Any) {
+        if useCostomSize.state != .on {
+            width.isEnabled = false
+            height.isEnabled = false
+        } else {
+            width.isEnabled = true
+            height.isEnabled = true
+        }
+    }
 }
 
